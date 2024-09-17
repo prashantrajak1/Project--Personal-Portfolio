34c79edaccb901a3757d28764e31e1a4ce0dbd09
PRASHANT RAJAK <prashantrajak1111@gmail.com>
2024-09-18 00:17:23 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
